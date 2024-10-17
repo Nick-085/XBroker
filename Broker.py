f764c7be01707f2bcfd0cdacc27d228a1ed97ff1
Nick Lloyd <nicholaslloyd085@gmail.com>
2024-10-17 01:28:11 +0000

    Get the IP address of the newly deployed VDS

--- Broker.py
+++ Broker.py
@@ -2,9 +2,11 @@
 
 import json
 import os
-import psycopg2
+# import psycopg2
+import re
 import subprocess
-from psycopg2 import sql
+import time
+# from psycopg2 import sql
 
 # Load env vars
 with open('config.json') as confFile:
@@ -27,40 +29,38 @@ vmToClone = config['xoSettings']['vmToClone']
 slowClone = config['xoSettings']['slowClone']
 
 # Register service user to XO(A)
-subprocess.run('xo-cli register --au ' + '--url ' + xo + " " + xo + " " + svcBrokerUser + " " + svcBrokerPass, shell=True)
+subprocess.run('xo-cli register --au --url ' + xo + ' ' + xo + ' ' + svcBrokerUser + ' ' + svcBrokerPass, shell=True)
 print(" ")
 
 # Clone VM and change its name to include the username of the requestor
-sessionUUID = capture_command_output('xo-cli vm.clone id=' + vmToClone + ' name=vds-' + rUser + ' full_copy=' + slowClone, shell=True)
+sessionUUID = capture_command_output('xo-cli vm.clone id=' + vmToClone + ' name=vds-' + rUser + ' full_copy=' + slowClone)
 subprocess.run('xo-cli vm.start id=' + sessionUUID, shell=True)
-userSession = subprocess.run('xo-cli vm.set id=' + sessionUUID + ' creation=' + rUser)
 sessionVMName = "vds-" + rUser
+time.sleep(45)
 
 # Gets the IP Address for the VM
-sessionIP_c = subprocess.run('xo-cli rest get vms/' + sessionUUID)
-sessionIP_j = json.loads(sessionIP_c)
-sessionIP = sessionIP_j['mainIpAddress']
-
+getMainIP = capture_command_output('xo-cli list-objects uuid=' + sessionUUID + ' | grep mainIpAddress')
+sessionIP = re.search(r'"mainIpAddress":\s*"([\d.]+)"', getMainIP).group(1)
 print(sessionIP)
 
-vdsRegister = 2 # add VDS to instance to user's Authentik/guac/openrport
+# vdsRegister = 2 # add VDS to instance to user's Authentik/guac/openrport
 
-# Connect to postgres db and add entry to user's connections
-conn = psycopg2.connect(
-    database = config['dbSettigs']['dbName'],
-    user = config['dbSettigs']['dbUser'],
-    password = config['dbSettigs']['dbPass'],
-    host = config['dbSettings']['dbHost'],
-    port = config['dbSettings']['dbPort']
-    )
+# # Connect to postgres db and add entry to user's connections
+# conn = psycopg2.connect(
+#     database = config['dbSettigs']['dbName'],
+#     user = config['dbSettigs']['dbUser'],
+#     password = config['dbSettigs']['dbPass'],
+#     host = config['dbSettings']['dbHost'],
+#     port = config['dbSettings']['dbPort']
+#     )
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
-insertQRY = sql.SQL("""
-        INSERT INTO guacamole_connection (connection_name, protocol)
-        VALUES (%s, %s)
-        RETURNING connection_id;
-    """)
+# insertQRY = sql.SQL("""
+#         INSERT INTO guacamole_connection (connection_name, protocol)
+#         VALUES (%s, %s)
+#         RETURNING connection_id;
+#     """)
 
-cur.execute(insertQRY, (sessionVMName, config['sessionSettings']['protocol']))
-connection_id = cur.fetchone()[0]
+# cur.execute(insertQRY, (sessionVMName, config['sessionSettings']['protocol']))
+# connection_id = cur.fetchone()[0]
